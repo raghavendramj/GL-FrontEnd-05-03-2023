f849c773cf1fd874e8977d761176c6bbbfdef61c
Raghavendra M J <raghavendramj123@gmail.com>
2023-04-16 17:40:01 +0530

    Initial code

--- JavaScript/Lab_v3/index.js
+++ JavaScript/Lab_v3/index.js
@@ -1,5 +1,25 @@
 const api = {
-    key: "7e3f21edee540e6110af347b55eb1ab2",
-    base: "https://api.openweathermap.org/data/2.5/"
-  }
-  
+  key: "7e3f21edee540e6110af347b55eb1ab2",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function getResults(query) {
+  let url = `${api.base}weather?q=${query}&units=metric&appid=${api.key}`;
+  //https://api.openweathermap.org/data/2.5/weather?q=Bengaluru&units=metric&appid=7e3f21edee540e6110af347b55eb1ab2
+  //   console.log(url);
+
+  fetch(url)
+    .then((weather) => weather.json())
+    .then((resp) => displayResults(resp));
+}
+function displayResults(weather) {
+  console.clear();
+  console.log("displayResults -> ", weather);
+  let cityHTML = document.querySelector(".city");
+  cityHTML.innerHTML = `${weather.name}`;
+
+  let now = new Date();
+  let dateHTML = document.querySelector(".location .date");
+}
+
+getResults("New Delhi");
