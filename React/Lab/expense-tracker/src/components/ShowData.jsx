5b0de3450626707ce07fa415bbce87da027381b1
Raghavendra M J <raghavendramj123@gmail.com>
2023-05-21 18:46:16 +0530

    Changes for show data

--- React/Lab/expense-tracker/src/components/ShowData.jsx
+++ React/Lab/expense-tracker/src/components/ShowData.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { getDataFromServer } from '../services/menu'
+import ExpenseTracker from "./ExpenseTracker";
 function ShowData() {
   const [items, setItems] = useState([]);
   const [sum, setSum] = useState();
-  const [rahul, setRahul] = useState();
-  const [ramesh, setRamesh] = useState();
+  const [rahulspent, setRahulspent] = useState();
+  const [rameshspent, setRameshspent] = useState();
+  const [showform, setShowForm] = useState();
 
   useEffect(() => {
     const fetchMenu = async () => {
@@ -18,7 +20,7 @@ function ShowData() {
       fetchShares(data);
     }
     fetchMenu();
-    
+
   });
 
   const fetchShares = (data) => {
@@ -26,28 +28,83 @@ function ShowData() {
     let a = 0, b = 0;
     console.log(data);
     for (let eachObj of data) {
-      
+
       if (eachObj.payeeName === 'Rahul') {
         a += eachObj.price;
       } else {
         b += eachObj.price;
       }
     }
-    setRahul(a);
-    setRamesh(b);
+    setRahulspent(a);
+    setRameshspent(b);
+  }
+
+  const success = () => {
+    setShowForm(true);
+  }
+
+  const cancel = () => {
+    setShowForm(false);
   }
 
   return (
-    <div className="">
-      <h1>Show Data Page</h1>
-      {items.map((each) => (
-        <li>{each.payeeName}</li>
-      ))}
-      <h1>Rahul :- {rahul}</h1>
-      <h1>Ramesh :- {ramesh}</h1>
-      <h1>Sum :- {sum}</h1>
+    // <div className="">
+    //   <h1>Show Data Page</h1>
+    //   {items.map((each) => (
+    //     <li>{each.payeeName}</li>
+    //   ))}
+    //   <h1>Rahul :- {rahul}</h1>
+    //   <h1>Ramesh :- {ramesh}</h1>
+    //   <h1>Sum :- {sum}</h1>
 
-    </div>
+    // </div>
+
+    <>
+      <header id="page-Header">Expense Tracker</header>
+      <button id="Add-Button" onClick={() => setShowForm(true)}>Add</button>
+      {showform && (
+        <div className="form">
+          <ExpenseTracker onTrue={success} onClose={cancel} />
+        </div>
+      )}
+
+      <>
+        <div className="use-inline date header-color">Date</div>
+        <div className="use-inline header-color">Product Purchased</div>
+        <div className="use-inline price header-color">Price</div>
+        <div className="use-inline header-color" style={{ width: 112 }}>
+          Payee
+        </div>
+      </>
+      {items &&
+        items.map((user, idx) => (
+          <div key={idx}>
+            <div className="use-inline date">{user.setDate}</div>
+            <div className="use-inline">{user.product}</div>
+            <div className="use-inline price">{user.price}</div>
+            <div className={`use-inline ${user.payeeName}`}>
+              {user.payeeName}
+            </div>
+          </div>
+        ))}``
+      <hr />
+      <div className="use-inline ">Total: </div>
+      <span className="use-inline total">{sum}</span> <br />
+      <div className="use-inline ">Rahul paid: </div>
+      <span className="use-inline total Rahul">{rahulspent}</span> <br />
+      <div className="use-inline ">Ramesh paid: </div>
+      <span className="use-inline total Ramesh">{rameshspent}</span> <br />
+      <span className="use-inline payable">
+        {rahulspent > rameshspent ? "Pay Rahul " : "Pay Ramesh"}
+      </span>
+      <span className="use-inline payable price">
+        {" "}
+        {Math.abs((rahulspent - rameshspent) / 2)}
+      </span>``
+    </>
+
+
+
   );
 }
 
